d1cf2e9704872708f481d209fce94c7ad311950d
zhang-zx <zhangzx15@126.com>
2019-04-10 15:32:09 +0800

    vgg11

--- lib/models/basic.py
+++ lib/models/basic.py
@@ -5,11 +5,14 @@
 # LICENSE file in the root directory of this source tree.
 #
 from .cpm_vgg16 import cpm_vgg16
+from .cpm_vgg11 import cpm_vgg11
 from .LK import LK
 
 def obtain_model(configure, points):
   if configure.arch == 'cpm_vgg16':
     net = cpm_vgg16(configure, points)
+  elif configure.arch == 'cpm_vgg11':
+    net = cpm_vgg11(configure, points)
   else:
     raise TypeError('Unkonw type : {:}'.format(configure.arch))
   return net
