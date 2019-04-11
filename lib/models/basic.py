97f69946eb8d8dfb1d436ab907ab66f7f6fbd2a2
Zhixing Zhang <zhangzx15@126.com>
2019-04-11 17:13:50 +0800

    debug

--- lib/models/basic.py
+++ lib/models/basic.py
@@ -7,12 +7,15 @@
 from .cpm_vgg16 import cpm_vgg16
 from .cpm_vgg11 import cpm_vgg11
 from .LK import LK
+from .cpm_small import cpm_mobileNet
 
 def obtain_model(configure, points):
   if configure.arch == 'cpm_vgg16':
     net = cpm_vgg16(configure, points)
   elif configure.arch == 'cpm_vgg11':
     net = cpm_vgg11(configure, points)
+  elif configure.arch == 'cpm_mobile':
+    net = cpm_mobileNet(configure, points)
   else:
     raise TypeError('Unkonw type : {:}'.format(configure.arch))
   return net
