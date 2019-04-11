c0eb20048c413cf5a633c4857fd84ef8dde35da4
Zhixing Zhang <zhangzx15@126.com>
2019-04-11 13:30:40 +0800

    reformat

--- scripts/demo_sbr-vgg11.sh
+++ scripts/demo_sbr-vgg11.sh
@@ -1,13 +1,17 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 CUDA_VISIBLE_DEVICES=2,3 python ./exps/lk_main.py \
-	--train_lists ./cache_data/lists/demo/demo-sbr.lst \
+	--train_lists ./cache_data/lists/300VW/300VW.train.lst \
 	              ./cache_data/lists/300W/300w.train.DET \
 	--eval_ilists ./cache_data/lists/demo/demo-sbr.lst \
+	              ./cache_data/lists/300VW/300VW.test-1.lst \
+	              ./cache_data/lists/300VW/300VW.test-2.lst \
+	              ./cache_data/lists/300VW/300VW.test-3.lst \
 	--num_pts 68 \
 	--model_config ./configs/Detector-vgg11.config \
 	--opt_config   ./configs/LK.SGD.config \
 	--lk_config    ./configs/mix.lk.config \
-	--video_parser x-1-1 --save_path ./snapshots/CPM-SBR-vgg11 \
+	--video_parser x-1-1 \
+	--save_path ./snapshots/CPM-SBR-vgg11 \
 	--init_model ./snapshots/300W-CPM-DET-vgg11/checkpoint/cpm_vgg11-epoch-049-050.pth  \
 	--pre_crop_expand 0.2 --sigma 4 \
 	--batch_size 8 --crop_perturb_max 5 --scale_prob 1 --scale_min 1 --scale_max 1 --scale_eval 1 --heatmap_type gaussian \
