3743142d62d821e62a15c6e108f5b39e280fe908
Zhixing Zhang <zhangzx15@126.com>
2019-04-15 14:27:45 +0800

    debug

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -1,3 +1,3 @@
-python ./exps/visual.py --meta snapshots/CPM-SBR-vgg11/metas/ --model eval-epoch-049-050-00-01.pth --save cache_data/cache/vgg11
-ffmpeg -start_number 3 -i cache_data/cache/vgg11/image%04d.png -b:v 30000k -vf "fps=30" -pix_fmt yuv420p cache_data/cache/vgg11.mp4
-cp cache_data/cache/vgg11.mp4 ../.jupyter
+python ./exps/visual.py --meta snapshots/CPM-SBR-small/metas/ --model eval-epoch-049-050-00-01.pth --save cache_data/cache/small
+ffmpeg -start_number 3 -i cache_data/cache/small/image%04d.png -b:v 30000k -vf "fps=30" -pix_fmt yuv420p cache_data/cache/small.mp4
+cp cache_data/cache/small.mp4 ../.jupyter
