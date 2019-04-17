f67eaad93a7a4b02871daaba1b9479f2325e26cf
Zhixing Zhang <zhangzx15@126.com>
2019-04-17 14:51:12 +0800

    debug

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -1,3 +1,3 @@
-python ./exps/visual.py --begin snapshots/test/metas/eval-begin-eval-00-02.pth --last snapshots/test/metas/eval-last-eval-00-02.pth --save cache_data/cache/test
-ffmpeg -start_number 3 -i cache_data/cache/test/image%04d.png -b:v 30000k -vf "fps=30" -pix_fmt yuv420p cache_data/cache/test.mp4
-cp cache_data/cache/test.mp4 ../.jupyter
+python ./exps/visual.py --begin snapshots/test/metas/eval-begin-eval-00-02.pth --last snapshots/test/metas/eval-last-eval-00-02.pth --save cache_data/cache/test-sbr
+ffmpeg -start_number 3 -i cache_data/cache/test-sbr/image%04d.png -b:v 30000k -vf "fps=30" -pix_fmt yuv420p cache_data/cache/test-sbr.mp4
+cp cache_data/cache/test-sbr.mp4 ../.jupyter
